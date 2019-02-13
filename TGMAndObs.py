d7649e5ee26f36c53270ef26b633cb22ad8f7e79
Jenny <jennyf@uow.edu.au>
2019-02-13 13:21:38 +1100

    TGM and observations updated to include different levels

--- TGMAndObs.py
+++ TGMAndObs.py
@@ -12,36 +12,47 @@ from sklearn.metrics import r2_score
 
 def SurfaceObsTGM(Old_Dataset, New_Dataset):
     
-     # Read in the data for the observed sites
-    AnHgObs= pd.read_csv('data/TGMSiteAnnual.csv',skiprows=[0], na_values=(-9999))
+    # Read in the data for the observed sites
+    AnHgObs= pd.read_csv('~/Documents/HG/data/TGMSiteAnnual.csv',skiprows=[0], na_values=(-9999))
     AnHgObs.columns=['SiteID', 'Lat', 'Lon','Alt', 'TGM', 'Hg0']
-    
     # Set levels for the colorbar in order to have a nonlinear scale.
     Levels= (0.75, 0.95, 1.15, 1.35, 1.55, 1.75, 2.30, 2.90, 3.50)
+    SiteID=AnHgObs.SiteID
     
     # Make a variable for the unit conversion factor to obtain ng/m^3
     Unit_Conversion= 8.93
+    def levels(SiteID):
+        level = {
+        'ZEP': 3,
+        'AND': 2,
+        'MWA': 1,
+        'MLO': 18,
+        'MBO': 16,
+        'NamCo': 18,
+        'LLN':16,
+            }
+        return level.get(SiteID.upper(), 0)
+    SiteID=AnHgObs.SiteID
+        # Extract and add together Hg0 and Hg2 at the surface from the reference model multiplying by the unit converion factor 
+        # to obtain values for Total Gaseous Mercury.
+    for i in range (len(SiteID)):
+        OLD_Hg0 =((Old_Dataset['IJ_AVG_S_Hg0'].isel(lev=levels(SiteID[i])).mean('time')) * Unit_Conversion)                              
+        OLD_Hg2 =((Old_Dataset['IJ_AVG_S_Hg2'].isel(lev=levels(SiteID[i])).mean('time')) * Unit_Conversion)                
+        TGM_Old = (OLD_Hg0 + OLD_Hg2)
     
-    # Extract and add together Hg0 and Hg2 at the surface from the reference model multiplying by the unit converion factor 
-    # to obtain values for Total Gaseous Mercury.
-    OLD_Hg0 =((Old_Dataset['IJ_AVG_S_Hg0'].isel(lev=0).mean('time')) * 		Unit_Conversion)                              
-    OLD_Hg2 =((Old_Dataset['IJ_AVG_S_Hg2'].isel(lev=0).mean('time')) * Unit_Conversion)                
-    TGM_Old = (OLD_Hg0 + OLD_Hg2)
     
-    
-    # Extract and add together Hg0 and Hg2 at the surface from the new model multiplying by the unit converion factor 
-    # to obtain values for Total Gaseous Mercury.
-    NEW_Hg0 =((New_Dataset['IJ_AVG_S_Hg0'].isel(lev=0).mean('time') * Unit_Conversion))                         
-    NEW_Hg2 =((New_Dataset['IJ_AVG_S_Hg2'].isel(lev=0).mean('time') * Unit_Conversion))
-    TGM_New = NEW_Hg0 + NEW_Hg2
-
+        # Extract and add together Hg0 and Hg2 at the surface from the new model multiplying by the unit converion factor 
+        # to obtain values for Total Gaseous Mercury.
+        NEW_Hg0 =((New_Dataset['IJ_AVG_S_Hg0'].isel(lev=levels(SiteID[i])).mean('time') * Unit_Conversion))                         
+        NEW_Hg2 =((New_Dataset['IJ_AVG_S_Hg2'].isel(lev=levels(SiteID[i])).mean('time') * Unit_Conversion))
+        TGM_New = NEW_Hg0 + NEW_Hg2
     
         
     # Find the absolute difference between the reference and new model.
     Abs_diff = TGM_New - TGM_Old
     # Find the absolute maximum value of the absolute difference. 
     Abs_MaxVal= np.max(np.abs(Abs_diff))
-    
+        
     
     # Find the percent difference of the models.  
     Perc_diff = (Abs_diff / TGM_Old)*100
@@ -109,7 +120,7 @@ def SurfaceObsTGM(Old_Dataset, New_Dataset):
     
     # Plot the reference model on the projection.
     im=TGM_Old.plot.contourf( x='lon',y='lat', ax=ax, levels= Levels, transform=ccrs.PlateCarree(), cmap='viridis', 
-                             cbar_kwargs={'orientation':'horizontal',
+                                 cbar_kwargs={'orientation':'horizontal',
                                            'ticklocation':'auto',
                                       'label':"Not Linear $ng/m^3$ "})
     
@@ -120,9 +131,9 @@ def SurfaceObsTGM(Old_Dataset, New_Dataset):
 
     # Add the observed values to the plot.
     plt.scatter(Long, Lati,  transform=ccrs.PlateCarree(),marker='D',
-            norm=colors.BoundaryNorm(boundaries=Levels, ncolors=256), 
-            linewidths=0.5, edgecolors='black',
-            label=None, c=Value, cmap='viridis')
+                norm=colors.BoundaryNorm(boundaries=Levels, ncolors=256), 
+                linewidths=0.5, edgecolors='black',
+                label=None, c=Value, cmap='viridis')
 
     # Add a title.
     plt.title(' Reference Model Version: Surface TGM',fontsize=15)   
@@ -133,6 +144,41 @@ def SurfaceObsTGM(Old_Dataset, New_Dataset):
     # Show the plot.
     plt.show()
     
+
+    
+    # Add a figure.
+    NEWMAP = plt.figure()
+    
+    # Add a geographical projection on the map.
+    ax = NEWMAP.add_subplot(111, projection=ccrs.PlateCarree())
+    
+    # Plot the new model on the projection.
+    im=TGM_New.plot.contourf(x='lon',y='lat',levels=Levels, ax=ax,transform=ccrs.PlateCarree(), cmap='viridis', 
+                         cbar_kwargs={'orientation':'horizontal',
+                                      'ticklocation':'auto',
+                                      'label':"Not Linear $ng/m^3$ "})
+    
+    # Add text to the plot.
+    plt.text(200,-50,textstr1, fontsize=14)
+    plt.text(200,-75,textstr3, fontsize=14)
+    plt.text(200,-100,textstr5, fontsize=14)
+    
+    # Add the observed values to the plot.
+    plt.scatter(Long, Lati,  transform=ccrs.PlateCarree(),marker='D',
+                norm=colors.BoundaryNorm(boundaries=Levels, ncolors=256), 
+                linewidths=0.75, edgecolors='black',
+                label=None, c=Value, cmap='viridis')
+
+    # Add a title.
+    plt.title(' New Model Version: Surface TGM', fontsize=15)       
+    
+    # Show the coastlines.
+    ax.coastlines()
+    
+    # Show the plot.
+    plt.show()  
+    
+    return (OLDMAP, NEWMAP)
 
     
     # Add a figure.
